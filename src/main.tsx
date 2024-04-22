b2f6fb05bd89cc7d9d67f06d7736466e5da3fe7b
Johan Rydell <johannilsrydell89@gmail.com>
2024-04-22 15:32:30 +0200

    added the routing for start, monsters and add monster

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AddMonster } from "./pages/AddMonster";
+import { Monster } from "./pages/Monster";
+import { Monsters } from "./pages/Monsters";
+import { Navigation } from "./pages/Navigation";
+import { NotFound } from "./pages/NotFound";
+import { Start } from "./pages/Start";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigation />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Start />,
+      },
+      {
+        path: "/start",
+        element: <Start />,
+      },
+      {
+        path: "/monsters",
+        element: <Monsters />,
+      },
+      {
+        path: "/addMonster",
+        element: <AddMonster />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
