f1256353fb9b84cf0539629f38c7c5a682499a45
Johan Rydell <johannilsrydell89@gmail.com>
2024-04-24 15:29:37 +0200

    added totals to Start

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,7 +1,31 @@
+import { useContext } from "react";
+import { MonsterContext } from "../state/MonsterStateContext";
+import { calculateTotals } from "../utils/calculateTotals";
+
 export const Start = () => {
+  const { state } = useContext(MonsterContext);
+
+  const {
+    totalMonsters,
+    totalEyes,
+    totalArms,
+    totalHorns,
+    totalWings,
+    totalTentacles,
+    totalMouths,
+    totalTails,
+  } = calculateTotals(state.monsters);
   return (
     <div>
-      <h2>Monster Univeristy!</h2>
+      <h2>Monster University!</h2>
+      <p>Total monsters: {totalMonsters}</p>
+      <p>Total eyes: {totalEyes}</p>
+      <p>Total arms: {totalArms}</p>
+      <p>Total horns: {totalHorns}</p>
+      <p>Total wings: {totalWings}</p>
+      <p>Total tentacles: {totalTentacles}</p>
+      <p>Total mouths: {totalMouths}</p>
+      <p>Total tails: {totalTails}</p>
     </div>
   );
 };
